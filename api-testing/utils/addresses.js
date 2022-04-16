0df8d6eff5191d1285181f2ea5747f3be42fc747
Vinicius <v.honorato@live.com>
2022-04-15 19:08:35 -0800

    fix(*): Parse zip to integer

--- api-testing/utils/addresses.js
+++ api-testing/utils/addresses.js
@@ -1,10 +1,10 @@
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
-class Addresses{
-    static street = faker.address.streetName();
-    static city = faker.address.city();
-    static state = faker.address.stateAbbr();
-    static zip = faker.address.zipCode("######");
+class Addresses {
+  static street = faker.address.streetName();
+  static city = faker.address.city();
+  static state = faker.address.stateAbbr();
+  static zip = parseInt(faker.address.zipCode("######"));
 }
 
 module.exports = Addresses;
